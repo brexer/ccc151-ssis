ff87bcad2dddc1168196e75adf1299e9590104ef
Bryle Jared Fantilanan <bjrfantilanan@gmail.com>
2025-02-20 04:44:49 +0800

    fixed icons, layout, add button

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from main_screen import Ui_MainWindow
 from PyQt6.QtWidgets import QApplication, QMainWindow, QStackedWidget
 import resources
 import sys
+import os
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+
 
         self.setWindowTitle("Student Information System")
 
@@ -13,19 +15,26 @@ class MainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-        with open("navbar.qss", "r") as style_file:
-            style_str = style_file.read()
-        app.setStyleSheet(style_str)
-
         self.ui.icon_only_widget.hide()
+        self.ui.widget_10.hide()
+        self.ui.widget_11.hide()
+        self.ui.widget_12.hide()
         self.ui.stackedWidget.setCurrentIndex(0)
+        self.ui.homeButton1.setChecked(True)
+        self.ui.homeButton2.setChecked(True)
 
+        self.ui.homeButton1.clicked.connect(lambda: self.change_page(0))
+        self.ui.homeButton2.clicked.connect(lambda: self.change_page(0))
         self.ui.studentButton1.clicked.connect(lambda: self.change_page(1))
         self.ui.studentButton2.clicked.connect(lambda: self.change_page(1))
         self.ui.programButton1.clicked.connect(lambda: self.change_page(2))
         self.ui.programButton2.clicked.connect(lambda: self.change_page(2))
         self.ui.collegeButton1.clicked.connect(lambda: self.change_page(3))
         self.ui.collegeButton2.clicked.connect(lambda: self.change_page(3))
+
+
+
+
 
 
 
@@ -41,5 +50,9 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
+
+    with open("navbar.qss", "r") as style_file:
+        style_str = style_file.read()
+        app.setStyleSheet(style_str)
 
     sys.exit(app.exec())
