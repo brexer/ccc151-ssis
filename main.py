8fd5ce537d8aa8f589fd84bb6e6b98c68d547090
Bryle Jared Fantilanan <bjrfantilanan@gmail.com>
2025-02-20 03:09:11 +0800

    layout fixed, sidebar working

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from main_screen import Ui_MainWindow
 from PyQt6.QtWidgets import QApplication, QMainWindow, QStackedWidget
+import resources
 import sys
 
 class MainWindow(QMainWindow):
@@ -7,10 +8,30 @@ class MainWindow(QMainWindow):
         super().__init__()
 
         self.setWindowTitle("Student Information System")
-        self.setGeometry(100, 100, 800, 600)
+
 
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+
+        with open("navbar.qss", "r") as style_file:
+            style_str = style_file.read()
+        app.setStyleSheet(style_str)
+
+        self.ui.icon_only_widget.hide()
+        self.ui.stackedWidget.setCurrentIndex(0)
+
+        self.ui.studentButton1.clicked.connect(lambda: self.change_page(1))
+        self.ui.studentButton2.clicked.connect(lambda: self.change_page(1))
+        self.ui.programButton1.clicked.connect(lambda: self.change_page(2))
+        self.ui.programButton2.clicked.connect(lambda: self.change_page(2))
+        self.ui.collegeButton1.clicked.connect(lambda: self.change_page(3))
+        self.ui.collegeButton2.clicked.connect(lambda: self.change_page(3))
+
+
+
+    def change_page(self, index):
+        self.ui.stackedWidget.setCurrentIndex(index)
+
 
 
 
